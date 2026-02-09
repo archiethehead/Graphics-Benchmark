89355d30d86a3d9268c2809bcfe28e2227a7d215
Archie Healy <archiethehead@gmail.com>
2026-02-09 19:16:38 +0000

    Window Creation
    Feat: Implemnted a main loop in the test-case, as well as seting up a main window with GLFW.
    
    Fix: Included glfw3.h

--- Source/main.c
+++ Source/main.c
@@ -1,11 +1,25 @@
 
+// OpenGL
 #include <GLEW/glew-2.1.0/Include/GL/glew.h>
 #include <GL/glu.h>
 #include <GL/gl.h>
+#include <GLFW/include/GLFW/glfw3.h>
 
+// Graphics Benchmark
 #include "graphicsBenchmark.h"
 
 int main() {
+
+	glfwInit();
+	GLFWwindow* window = glfwCreateWindow(640, 480, "My Title", NULL, NULL);
+	glfwMakeContextCurrent(window);
+
+	while (1) {
+	
+		timeFrame();
+		printStats();
+
+	}
 
 	return 0;
 
