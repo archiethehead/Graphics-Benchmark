53076f35501d89dce7892e134f6b1befddd01e15
Archie Healy <archiethehead@gmail.com>
2026-02-09 04:56:32 +0000

    Cross-Platform Time
    Feat: Added a macro in the main file that checks the current system definition to see if it's Windows, importing the appropriate system libraries if it is or not.
    
    Chore: Implemented the draw call counter and static variables.

--- Source/graphicsBenchmark.c
+++ Source/graphicsBenchmark.c
@@ -1,7 +1,22 @@
 #include "graphicsBenchmark.h"
+#include <time.h>
+
+#ifdef _WIN32
+
+#include <windows.h>
+static LARGE_INTEGER frequency, frameStartTime, frameEndTime;
+
+#else
+
+#include <time.h>
+static struct timespec frameStartTime, frameEndTime;
+
+#endif
 
 
-void drawCall() {
+static unsigned int drawCalls = 0;
+static double frameTime = 0.0;
+static int isFrame = 0;
 
 
-}
+void drawCall() { drawCalls++; }
