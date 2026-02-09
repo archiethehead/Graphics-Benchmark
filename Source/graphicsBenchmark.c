9a69509c4fbc2892c4293832abecad270f9f7bdc
Archie Healy <archiethehead@gmail.com>
2026-02-09 04:59:50 +0000

    Frame Begin
    Feat: Implemented a function that tunes variables to a state where a frame has begun it's lifecycle, with cross-platform macros in place.

--- Source/graphicsBenchmark.c
+++ Source/graphicsBenchmark.c
@@ -18,5 +18,23 @@ static unsigned int drawCalls = 0;
 static double frameTime = 0.0;
 static int isFrame = 0;
 
+void frameBegin() {
+
+	drawCalls = 0;
+
+#ifdef _WIN32
+
+	QueryPerformanceFrequency(&frequency);
+	QueryPerformanceCounter(&frameStartTime);
+
+#else
+
+	clock_gettime(CLOCK_MONOTONIC, &frameStartTime);
+
+#endif
+
+	isFrame = isFrame = 1;
+
+}
 
 void drawCall() { drawCalls++; }
