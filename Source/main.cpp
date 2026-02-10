663ca059cd6901648312300ca65f048733098a68
Archie Healy <archiethehead@gmail.com>
2026-02-10 19:32:38 +0000

    OpenGL Libs and C++
    Refactor: main.c is now main.cpp, favouring the GLM math library for the rendering example, and to display cross-platform/language capabilites.
    
    Fix: OpenGL libraries now properly included within repository.

---
+++ Source/main.cpp
@@ -0,0 +1,27 @@
+
+// OpenGL
+#include <GL/gl.h>
+#include <GLFW/include/GLFW/glfw3.h>
+#include <GLM/glm/glm.hpp>
+
+// Standard Libraries
+#include <math.h>
+#include <stdlib.h>
+#include <stddef.h>
+
+// Graphics Benchmark
+#include "graphicsBenchmark.h"
+
+typedef struct Vertext {
+
+	glm::vec2 pos;
+	glm::vec3 colour;
+
+} Vertex;
+
+
+int main() {
+
+	
+
+}
